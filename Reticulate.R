e457074d62b9813fffc96a7be21c3b85cdddc99f
gyurmalin <gyorgy.jablonszky@gmail.com>
2018-04-24 00:48:56 +0200

    testing reticulate on flights

--- Reticulate.R
+++ Reticulate.R
@@ -5,3 +5,19 @@
 install.packages("reticulate")
 
 library(reticulate)
+
+os <- import("os")
+os$listdir(".")
+
+repl_python()
+
+import pandas
+
+flights = pandas.read_csv("flights.csv")
+flights = flights[flights["dest"] == "ORD"]
+flights = flights[["carrier", "dep_delay", "arr_delay"]]
+flights = flights.dropna()
+exit
+
+library(ggplot2)
+ggplot(py$flights, aes(carrier, arr_delay)) + geom_point() + geom_jitter()
